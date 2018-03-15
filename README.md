394475008dacbc2c01c136ccbba5901d470cdee5
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-03-15 15:41:30 +0100

    finished our 3rd tutorial

--- README.md
+++ README.md
@@ -12,14 +12,20 @@ Each Campground has :
 [{ "name": "", "image": "" }, { "name": "", "image": "" }]
 ```
 
-# Layout and Basic Styling
+## Layout and Basic Styling
 
 * Create our header and footer partials
 * Add in bootstrap or Personal Styles
 
-# Creating New Campgrounds
+## Creating New Campgrounds
 
 * Setup new campground POST route
 * Add in body-parser
 * Setup route to show from
 * Add basic unstyled form
+
+## Style the campgrounds Page
+
+* Add a better header/title
+* Make campgrounds display in a grid
+
--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express()
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+
+const campgrounds =  [
+  {"name": "Salmon geek", "image": "https://farm4.staticflickr.com/3717/12822913975_18e8b3e69d.jpg" },
+    {"name": "Wakajaye hills", "image": "https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg" },
+    {"name": "abuja lugbe", "image": "https://farm1.staticflickr.com/316/20487726582_7ff68bfc92.jpg"}
+]
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -9,15 +17,22 @@ app.get('/', (req, res) => {
 
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds =  [
-    {"name": "Salmon geek", "image": "https://farm4.staticflickr.com/3717/12822913975_18e8b3e69d.jpg" },
-      {"name": "Wakajaye hills", "image": "https://farm8.staticflickr.com/7296/28070862692_32f82c02ba.jpg" },
-      {"name": "abuja lugbe", "image": "https://farm1.staticflickr.com/316/20487726582_7ff68bfc92.jpg"}
-]
 
   res.render('campgrounds', {
     campgrounds: campgrounds
   })
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new')
+});
+
+app.post('/campgrounds', (req, res) => {
+  const {name, image} = req.body;
+  console.log(name, image);
+  const newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect('campgrounds');
 });
 
 app.listen(3000, () => {
