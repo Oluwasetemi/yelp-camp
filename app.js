e000c11d1794e043b5857b0269a67813e6dbe7c9
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-07-10 11:51:30 +0100

    add update and delete to v2

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/yelp_camp");
+var methodOverride = require('method-override')
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 //Schema setup
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String, 
+  image: String,
   description: String
 });
 
@@ -22,7 +25,7 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //   description: 'It is a very wonderful place to visit, good toilet, network for internet: A safe haven'
 // }, (err, campground) => {
 //   if (err) {
-//     console.log(err); 
+//     console.log(err);
 //   } else {
 //     console.log('New data campground inserted to db');
 //     console.log(campground);
@@ -63,9 +66,45 @@ app.post("/campgrounds", (req, res) => {
 app.get('/campgrounds/:id', (req, res) => {
   Campground.findById(req.params.id, (err, foundCampground) => {
     if (err) {
-      console.log('err'); 
+      console.log('err');
     } else {
       res.render('show', {campground: foundCampground})
+    }
+  })
+});
+
+/* Update Route */
+app.get('/campgrounds/:id/edit', (req, res) => {
+  Campground.findById({_id: req.params.id}, (err, foundCampground) => {
+    if (err) {
+      console.log('err');
+    } else {
+      res.render('edit', {campground: foundCampground})
+    }
+  })
+});
+
+app.put('/campgrounds/:id', (req, res) => {
+  const {id} = req.params;
+  const {campground} = req.body;
+  // console.log({id, blog});
+  Campground.findByIdAndUpdate(id, campground, (err, updated) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect(`/campgrounds/${id}`);
+    }
+  })
+})
+
+/* Delete Route */
+app.delete('/campgrounds/:id', (req, res) => {
+  const {id} = req.params;
+  Campground.findByIdAndRemove(id, (err) => {
+    if (err) {
+      res.send('error');
+    } else {
+      res.redirect('/campgrounds');
     }
   })
 });
